9839aa6e75bbec3076716f0255bf10a3cc25f99c
wscats <2587575267@qq.com>
2017-02-27 07:48:49 +0800

    add vue demo

---
+++ vue-news/app/wheader.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="weui-tab">
+		<div class="weui-navbar">
+			<!--weui-bar__item_on-->
+			<a href="#/index/recommend/6" @click="tabs(6)" :class="{'weui-bar__item_on': tab==6}" class="weui-navbar__item">
+				推荐
+			</a>
+			<a href="#/index/hot/7" @click="tabs(7)" :class="{'weui-bar__item_on': tab==7}" class="weui-navbar__item">
+				热点
+			</a>
+			<a href="#/index/entertainment/8" @click="tabs(8)" :class="{'weui-bar__item_on': tab==8}" class="weui-navbar__item">
+				娱乐
+			</a>
+		</div>
+		<div class="weui-tab__panel">
+
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		replace: false,
+		data() {
+			return {
+				tab: 6
+			}
+		},
+		methods: {
+			tabs: function(tab) {
+				this.tab = tab;
+			}
+		},
+		components: {},
+		mounted() {
+		},
+		directives: {
+
+		}
+	}
+</script>
+<style scoped>
+	.weui-navbar a {
+		color: black;
+	}
+</style>
---
+++ vue-news/app/wsearch.vue
@@ -0,0 +1,45 @@
+<template>
+	<div :class="{'weui-search-bar_focusing':isSearch}" class="weui-search-bar" id="search_bar">
+		<form class="weui-search-bar__form">
+			<div class="weui-search-bar__box">
+				<i class="weui-icon-search"></i>
+				<input type="search" @focus="inSearch()" @keyup="search()" v-model="searchName" class="weui-search-bar__input" id="search_input" placeholder="搜索" />
+				<a href="javascript:" @click="cancel()" class="weui-icon-clear" id="search_clear"></a>
+			</div>
+			<label for="search_input" class="weui-search-bar__label" id="search_text">
+				<i class="weui-icon-search"></i>
+				<span>搜索</span>
+			</label>
+		</form>
+		<a href="javascript:" @click="cancel()" class="weui-search-bar__cancel-btn" id="search_cancel">取消</a>
+	</div>
+</template>
+<script>
+	export default {
+		replace: false,
+		data() {
+			return {
+				isSearch:false,
+				searchName:"Hello I'm wscat~"
+			}
+		},
+		methods: {
+			inSearch(){
+				this.isSearch = true
+			},
+			cancel(){
+				this.isSearch = false
+			},
+			search(){
+				this.$store.searchName = this.searchName;
+				console.log(this.$store)
+			}
+		},
+		mounted(){
+			console.log(this.$store)
+		}
+	}
+</script>
+<style scoped>
+
+</style>
